e834025dc48be3df251b57a1713df45e58aab3dd
xingwGitHub <18404972905@163.com>
2018-11-11 22:16:37 +0800

    tijiao

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -48,10 +48,11 @@ module.exports = {
         test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 1000,
           name: utils.assetsPath('img/[name].[hash:7].[ext]')
         }
       },
+
       {
         test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
         loader: 'url-loader',
